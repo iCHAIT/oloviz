ebe583262438446d794c8d7005e07c24e6cdbeb0
iCHAIT <cgupta319@gmail.com>
2018-05-13 18:23:46 +0800

    minor changes

--- get_foodpanda_olo.py
+++ get_foodpanda_olo.py
@@ -62,9 +62,9 @@ if __name__ == "__main__":
     # Store all orders in a list
     orders = []
 
-    # List storing cookies for all users
-    cookies = ['__cfduid=d0dc7bc4366a7088f77119d2e6d9885731526195069; hl=en; AppVersion=f0d686d; ld_key=121.7.170.113; ld_flags_sum=0b61dd24806ba2d3590b7c80700d795d; PHPSESSID=e336b8c03966421ac3e8651646f61df7; token=eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpZCI6IjE3MzczYzhmZjMxN2QxMDQ4NjE1MzBmYTdlM2NjNmZhOGNlYThhZTciLCJjbGllbnRfaWQiOiJ2b2xvIiwidXNlcl9pZCI6ImNndXB0YTMxOUBnbWFpbC5jb20iLCJleHBpcmVzIjoxNTU3MzA5OTU1LCJ0b2tlbl90eXBlIjoiYmVhcmVyIiwic2NvcGUiOiJBUElfQ1VTVE9NRVIgQVBJX1JFR0lTVEVSRURfQ1VTVE9NRVIifQ.vrpAD-SnmbfDejk90sfXi2J6fsKXsT2TeyZzBEzL_IeEAHM-oc3V7BLA3lw1sWGkQ4JPSSgFgKW4OFJI5J1hjD85Ilf64Rgx8FneU9NI5PyVw-XKkO3EH4UmhVuWv__KyXut9zWXq8nkYpGYJxmLgqExdFamOJ5B0uo-tN2SohrizhUp65ILfF7u5mDe0Kt6ls-cMOrpkCiiXHrm-hNyn7mZLXQvPGO4nVapgt9rRs9_Oncvm8M5rClDM96Jgo-cPnNwzJsuu-wjZDwuFYY8A-1zYbKC5zJn6_yrMJkMKmp87EufK6UvxBT4Nmcmqq3hkojwj1rWfi3_GcGVELSwMA; tooltip-reorder=true',
-               '__cfduid=d0dc7bc4366a7088f77119d2e6d9885731526195069; hl=en; AppVersion=f0d686d; ld_key=121.7.170.113; ld_flags_sum=0b61dd24806ba2d3590b7c80700d795d; tooltip-reorder=true; PHPSESSID=ec0c4cae50542d4f703f5ae1ed94bb0e; token=eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpZCI6IjkwYmEzZDNiZWVhMGZiM2ZjNGZlZWIzM2QwYjk2OTA2NjUyOTk1ZjgiLCJjbGllbnRfaWQiOiJ2b2xvIiwidXNlcl9pZCI6Im1lZ2huYTBzYXhlbmFAeWFob28uY28uaW4iLCJleHBpcmVzIjoxNTU3MzEwMTIzLCJ0b2tlbl90eXBlIjoiYmVhcmVyIiwic2NvcGUiOiJBUElfQ1VTVE9NRVIgQVBJX1JFR0lTVEVSRURfQ1VTVE9NRVIifQ.nzDnrpM5Tqkq4-1F1G-VxXsZTwXhTj78R6lOqgs8GymapyVFlIhYG1IeGqjcMSt83VwyV_Gjzaq-dMw3Kud-2ut4CjVfmJCxBR7e2nTPWuCS_XzURzdAxrIYoWava_Xlk3yloK2DUmSuC2EVn1GAhIh8x-lHnuHLbUF7iwrs2OClylVqok7I-KHcd-N0Z_VKMQUX-qmcj-MxfcA0EHVxP17a1Ax91Ek0mV-Eo_M5Q2yIQ-_-W0LFuOHN0uNN1R9iZr2W2dwJ3-uDvNzT-CIpLHn-gxooohEmwpbNAlQO_5AilVTAJDbySWOLSMTk3Lal3WlLo5qBUgQH0wV4xk_xjg; rider-banner-hidden=1']
+    # Store cookies for all your users in this python list
+    # If only 1 user then just place cookie for that user
+    cookies = ['cookie for user 1', 'cookies for user 2']
 
     # Get foodpanda order for all users
     for cookie in cookies:
